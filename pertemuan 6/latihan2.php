b30ed284ba5ae2106f4bdcc7200c8cae84f530a8
ibnuajisentana584-code <ibnuajisentana584@gmail.com>
2026-09-25 14:16:08 +0700

    Add files via upload

---
+++ pertemuan 6/latihan2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan 2 - Array Foreach</title>
+</head>
+<body>
+
+<?php
+$x = array("one", "two", "three");
+
+foreach ($x as $value) {
+    echo $value . "<br />";
+}
+?>
+
+</body>
+</html>
